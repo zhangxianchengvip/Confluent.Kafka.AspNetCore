af93f320a43e3a6cd2d81d9ed03c0aa94a3717d9
zhangxiancheng <365839159@qq.com>
2023-07-17 22:26:22 +0800

    extension

---
+++ src/Confluent.Kafka.AspNetCore/Extensions/ISubscribeHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Confluent.Kafka.AspNetCore.Extensions
+{
+    public interface ISubscribeHandler<TValue>
+    {
+        Task Handle(TValue value);
+    }
+}
+
---
+++ src/Confluent.Kafka.AspNetCore/Extensions/SubscribeAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Confluent.Kafka.AspNetCore.Extensions
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class SubscribeAttribute : Attribute
+    {
+        public string Topic { get; set; }
+        public SubscribeAttribute(string topic)
+        {
+            Topic = topic;
+        }
+    }
+}
